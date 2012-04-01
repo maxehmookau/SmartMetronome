b97ff4c200d668cb23e5ddd20afdb1d520734556
Max Woolf <max@zingysaturn.co.uk>
2012-04-01 11:02:09 +0100

    Initial Commit

---
+++ SmartMetronome/SMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SMAppDelegate.h
+//  SmartMetronome
+//
+//  Created by Max Woolf on 01/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmartMetronome/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SmartMetronome
+//
+//  Created by Max Woolf on 01/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMAppDelegate class]));
+    }
+}
